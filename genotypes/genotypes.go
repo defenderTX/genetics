387642f8273257234694fa266f023fe16f1c11dd
Michael Fox <michaelfox@Elizabeths-MacBook-Pro.local>
2015-08-08 20:23:40 -0500

    Initial commit

---
+++ genotypes/genotypes.go
@@ -0,0 +1,11 @@
+package genotypes
+
+var CHROMOSOME_LENGTH = 9
+
+type Genotype struct {
+	chromosome string
+}
+
+func (genotype Genotype) GetFormula() string {
+
+}
---
+++ parser/parser.go
@@ -0,0 +1,26 @@
+package parser
+
+import (
+	"genetics/genes"
+	"genetics/genotypes"
+)
+
+type tree struct {
+	left *tree
+	token string
+	right *tree
+}
+
+func Evaluate(chromosome string) int32 {
+
+}
+
+func buildAst(chromosome string) tree {
+	values := ""
+	for i := 0; i < len(chromosome); i += genes.GENE_LENGTH {
+		values += genes.GeneToValue(chromosome[i:i + GENE_LENGTH])
+		values += " "
+	}
+}
+
+func parseFormula()
