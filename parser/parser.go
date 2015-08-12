5fe1da24fce900e62c8be05559e2fcdaa20b97d6
Michael Fox <mswfox@gmail.com>
2015-08-11 20:43:20 -0500

    Removed stale code from parser

--- parser/parser.go
+++ parser/parser.go
@@ -1,23 +1,7 @@
 package parser
 
-import "github.com/defendertx/genetics/genes"
-
 type tree struct {
 	left  *tree
 	token string
 	right *tree
 }
-
-func Evaluate(chromosome string) int32 {
-
-}
-
-func buildAst(chromosome string) tree {
-	values := ""
-	for i := 0; i < len(chromosome); i += genes.GENE_LENGTH {
-		values += genes.GeneToValue(chromosome[i : i+GENE_LENGTH])
-		values += " "
-	}
-}
-
-func parseFormula()
